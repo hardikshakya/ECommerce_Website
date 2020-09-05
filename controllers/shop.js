6b2d2646f4769f8ccbbbd15d1bafb16a3050e7d1
hardikshakya <hardikshakya1997@gmail.com>
2020-09-05 15:34:21 +0530

    B7T1: Added session and cookies - Used express-session package - Add stored session in mongodb database using connect-mongodb-session

--- controllers/shop.js
+++ controllers/shop.js
@@ -9,6 +9,7 @@ exports.getProducts = (req, res, next) => {
                 prods: products,
                 pageTitle: "All Products",
                 path: "/products",
+                isAuthenticated: req.session.isLoggedIn,
             });
         })
         .catch((err) => {
@@ -25,6 +26,7 @@ exports.getProduct = (req, res, next) => {
                 product: product,
                 pageTitle: product.title,
                 path: "/products",
+                isAuthenticated: req.session.isLoggedIn,
             });
         })
         .catch((err) => console.log(err));
@@ -38,6 +40,7 @@ exports.getIndex = (req, res, next) => {
                 prods: products,
                 pageTitle: "Shop",
                 path: "/",
+                isAuthenticated: req.session.isLoggedIn,
             });
         })
         .catch((err) => {
@@ -56,6 +59,7 @@ exports.getCart = (req, res, next) => {
                 path: "/cart",
                 pageTitle: "Your Cart",
                 products: products,
+                isAuthenticated: req.session.isLoggedIn,
             });
         })
         .catch((err) => console.log(err));
@@ -122,6 +126,7 @@ exports.getOrders = (req, res, next) => {
                 path: "/orders",
                 pageTitle: "Your Orders",
                 orders: orders,
+                isAuthenticated: req.session.isLoggedIn,
             });
         })
         .catch((err) => console.log(err));
