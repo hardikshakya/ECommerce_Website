515e07314a501b575a20309899d3bc83aa24fbd0
hardikshakya <hardikshakya1997@gmail.com>
2020-09-06 21:12:39 +0530

    B10T1: Added img Upload & PDF Download feature - Able to upload image - Able to download Invoice PDF

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,6 +1,7 @@
 const { validationResult } = require("express-validator");
 
 const Product = require("../models/product");
+const fileHelper = require("../util/file");
 
 exports.getAddProduct = (req, res, next) => {
     res.render("admin/edit-product", {
@@ -16,20 +17,35 @@ exports.getAddProduct = (req, res, next) => {
 // POST /add-product
 exports.postAddProduct = (req, res, next) => {
     const title = req.body.title;
-    const imageUrl = req.body.imageUrl;
+    const image = req.file;
     const price = req.body.price;
     const description = req.body.description;
-    const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
+    if (!image) {
         return res.status(422).render("admin/edit-product", {
             pageTitle: "Add Product",
-            path: "/admin/edit-product",
+            path: "/admin/add-product",
             editing: false,
             hasError: true,
             product: {
                 title: title,
-                imageUrl: imageUrl,
+                price: price,
+                description: description,
+            },
+            errorMessage: "Attached file is not an image.",
+            validationErrors: [],
+        });
+    }
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).render("admin/edit-product", {
+            pageTitle: "Add Product",
+            path: "/admin/add-product",
+            editing: false,
+            hasError: true,
+            product: {
+                title: title,
                 price: price,
                 description: description,
             },
@@ -38,6 +54,7 @@ exports.postAddProduct = (req, res, next) => {
         });
     }
 
+    const imageUrl = image.path;
     const product = new Product({
         title: title,
         price: price,
@@ -92,9 +109,8 @@ exports.postEditProduct = (req, res, next) => {
     const prodId = req.body.productId;
     const updatedTitle = req.body.title;
     const updatedPrice = req.body.price;
-    const updatedImageUrl = req.body.imageUrl;
+    const image = req.file;
     const updatedDesc = req.body.description;
-
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
@@ -105,7 +121,6 @@ exports.postEditProduct = (req, res, next) => {
             hasError: true,
             product: {
                 title: updatedTitle,
-                imageUrl: updatedImageUrl,
                 price: updatedPrice,
                 description: updatedDesc,
                 _id: prodId,
@@ -123,7 +138,10 @@ exports.postEditProduct = (req, res, next) => {
             product.title = updatedTitle;
             product.price = updatedPrice;
             product.description = updatedDesc;
-            product.imageUrl = updatedImageUrl;
+            if (image) {
+                fileHelper.deleteFile(product.imageUrl);
+                product.imageUrl = image.path;
+            }
             return product.save().then((result) => {
                 console.log("UPDATED PRODUCT!");
                 res.redirect("/admin/products");
@@ -158,7 +176,14 @@ exports.getProducts = (req, res, next) => {
 // POST /delete-product
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
-    Product.findByIdAndRemove({ _id: prodId, userId: req.user._id })
+    Product.findById(prodId)
+        .then((product) => {
+            if (!product) {
+                return next(new Error("Product not found."));
+            }
+            fileHelper.deleteFile(product.imageUrl);
+            return Product.deleteOne({ _id: prodId, userId: req.user._id });
+        })
         .then(() => {
             console.log("DESTROYED PRODUCT");
             res.redirect("/admin/products");
